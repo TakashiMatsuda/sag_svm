a9d0242da2b1c010d9dade0570c2654956bfa935
MatsudaTakashi <mazda.takasky@gmail.com>
2016-03-07 23:18:17 +0900

    implemented find_sv, but not tested

--- write_sv.py
+++ write_sv.py
@@ -3,15 +3,27 @@
 import numpy as np
 
 
-def find_sv():
+def find_sv(r, y):
+    svlist = []
+    for i, y_i in enumerate(y):
+        if (r[i] * y_i) == 0:
+            svlist.append(i)
+
+    return svlist
 
 
-
-def write_sv(svx, svy, svr):
+def write_sv(x, y, r, svlist):
+    svx = [x[p] for p in svlist]
+    svy = [y[p] for p in svlist]
+    svr = [r[p] for p in svlist]
     np.save('svx.npy', svx)
     np.save('svy.npy', svy)
-    np.save('svr.npy', svy)
+    np.save('svr.npy', svr)
     return 0
+
+
+def test_find_sv():
+    assert 0
 
 
 def test_write_sv():
