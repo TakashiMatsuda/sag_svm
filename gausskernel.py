8a61274dd349ddfc56a8912ece92be225b6db56f
MatsudaTakashi <mazda.takasky@gmail.com>
2016-03-07 18:07:10 +0900

    implemented gausskernel

--- gausskernel.py
+++ gausskernel.py
@@ -1,18 +1,24 @@
 #!/Users/takashi/.pyenv/shims/python
 
 import numpy as np
+import math
 
 
 def gausskernel(x1, x2):
     """
         x1, x2 must be numpy's ndarrays.
+        This code returns a gaussian kernel value between x1 and x2
     """
-    return np.exp(- np.square(np.subtract(x1, x2)) / 2)
+    subtr = np.subtract(x1, x2)
+    print(subtr)
+    return math.exp(- np.inner(subtr, subtr) / 2)
 
 
 def test_gausskernel():
     tx1 = np.array([1, 0])
     tx2 = np.array([1, 0])
     sol = gausskernel(tx1, tx2)
-    print("gausskernel: " + str(sol) + " between " + str(tx1) + " and " + str(tx2))
+    print("gausskernel: " + str(sol) +
+          " between " + str(tx1) + " and " + str(tx2))
+
     assert sol == 0
