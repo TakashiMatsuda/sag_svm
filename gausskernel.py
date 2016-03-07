80023b4ea82510f4da871d1fe3204edb516dcf9a
MatsudaTakashi <mazda.takasky@gmail.com>
2016-03-07 17:58:41 +0900

    implemented gausskernel

--- gausskernel.py
+++ gausskernel.py
@@ -1,8 +1,13 @@
 #!/Users/takashi/.pyenv/shims/python
 
+import numpy as np
+
 def gausskernel(x1, x2):
+    """
+        x1, x2 must be numpy's ndarrays.
+    """
+    return np.exp(- np.square(np.subtract(x1, x2)) / 2)
 
 
 def test_gausskernel():
     tx1, tx2 = 0
-    
