66ea681f6546c53c6a4d08fade2202858123b584
MatsudaTakashi <mazda.takasky@gmail.com>
2016-03-08 19:38:37 +0900

    make_kmx passed the test@

--- make_kmx.py
+++ make_kmx.py
@@ -2,14 +2,29 @@
 
 import numpy as np
 import gausskernel
+from functools import partial
 
 
 def make_kmx(x_vec, ker):
+
     kmx = np.array([[ker(x_i, x_j) for x_j in x_vec] for x_i in x_vec])
+
+    """
+    だから、このように分解してみる。
+    """
+    """
+    kmx = []
+    for x_i in x_vec[:]:
+        for x_j in x_vec[:]:
+            kmx.append(ker(x_i, x_j))
+            テストが間違っていることが判明した。
+    """
+    print(kmx)
     return kmx
 
 
 def test_make_kmx():
-    x_vec = [[1. for i in range(10)]]
+    x_vec = [[1. for i in range(3)] for j in range(5)]
     sol = make_kmx(x_vec, gausskernel.gausskernel)
+    print('sol : ' + str(sol))
     assert sol[2, 2] == 1
