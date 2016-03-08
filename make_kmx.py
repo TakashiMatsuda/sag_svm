67510da38a24be5e3d72c4300081954cbc01a15a
MatsudaTakashi <mazda.takasky@gmail.com>
2016-03-08 20:47:50 +0900

    removed funny comments

--- make_kmx.py
+++ make_kmx.py
@@ -7,16 +7,6 @@ from functools import partial
 
 def make_kmx(x_vec, ker):
     kmx = np.array([[ker(x_i, x_j) for x_j in x_vec] for x_i in x_vec])
-    """
-    だから、このように分解してみる。
-    """
-    """
-    kmx = []
-    for x_i in x_vec[:]:
-        for x_j in x_vec[:]:
-            kmx.append(ker(x_i, x_j))
-            テストが間違っていることが判明した。
-    """
     return kmx
 
 
