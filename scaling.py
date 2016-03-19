19c5f254f63cd2b367266d2971d18e1789cdf4de
MatsudaTakashi <mazda.takasky@gmail.com>
2016-03-19 15:06:00 +0900

    scaling passed the average test

--- scaling.py
+++ scaling.py
@@ -1,16 +1,25 @@
 #!/Users/takashi/.pyenv/shims/python
 
+import numpy as np
+
 
 def scaling(data):
     """
         Scaling. Make x's average to 0, variance to 1
     """
+    scaled_data = np.zeros_like(data)
+    sumlist = np.sum(data, axis=0)
+    avglist = np.array([d / len(data[0]) for d in sumlist])
+    for i, x in enumerate(data):
+        scaled_data[i] = np.array([x[j] - avglist[j] for j in range(len(x))])
+
+    print(scaled_data)
     return scaled_data
 
 
 def test_scaling():
-    import numpy
-    import numpy.randam as rd
+    from numpy import random as rd
     data = [[rd.rand() * j for i in range(5)] for j in range(5)]
     res = scaling(data)
-    assert res.average == 0.
+    print(res)
+    assert np.sum(res)
