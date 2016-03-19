c9c537ebd0f3204b376c911ab8e4d120a7bbfa3d
MatsudaTakashi <mazda.takasky@gmail.com>
2016-03-19 15:18:53 +0900

    write scaling variance

--- scaling.py
+++ scaling.py
@@ -1,6 +1,7 @@
 #!/Users/takashi/.pyenv/shims/python
 
 import numpy as np
+import math
 
 
 def scaling(data):
@@ -8,21 +9,32 @@ def scaling(data):
         Scaling. Make x's average to 0, variance to 1
     """
     scaled_data = np.zeros_like(data)
+    """
+        average section
+    """
     sumlist = np.sum(data, axis=0)
     avglist = np.array([d / len(data[0]) for d in sumlist])
     for i, x in enumerate(data):
         scaled_data[i] = np.array([x[j] - avglist[j] for j in range(len(x))])
 
+    """
+        variance section
+    """
+    vr = math.sqrt(np.sum(np.square(scaled_data)))
+    scaled_data = np.array([x / vr for x in scaled_data])
     print(scaled_data)
     return scaled_data
 
 
 def test_scaling():
-    from numpy import random as rd
-    data = [[rd.rand() * j for i in range(5)] for j in range(5)]
+    data = [[i * j for i in range(5)] for j in range(5)]
     res = scaling(data)
     print(res)
     """
         average test
     """
-    assert (np.sum(res))[0] == 0
+    assert np.sum(res, axis=0)[3] == 0
+    """
+        variance test
+    """
+    assert 0
